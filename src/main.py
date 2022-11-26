4da07cafdfa6fd142d7bada0d0d0b81faeef795c
Eugene-Shcherbak <yevhen.shcherbak.kn.2021@lpnu.ua>
2022-11-26 16:48:38 +0200

    added tests

--- src/main.py
+++ src/main.py
@@ -9,21 +9,19 @@ from flask_httpauth import HTTPBasicAuth
 from src.error_handler.exception_wrapper import handle_error_format
 from src.error_handler.exception_wrapper import handle_server_exception
 
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:fojdb67332#@localhost:3306/shop'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# @app.before_request
+# def create_tables():
+# db.drop_all()
+# db.create_all()
+# db.session.commit()
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 auth = HTTPBasicAuth()
-
-
-#@app.before_request
-#def create_tables():
-    #db.drop_all()
-    #db.create_all()
-    #db.session.commit()
 
 
 @auth.verify_password
@@ -96,7 +94,7 @@ class Product(db.Model):
     @classmethod
     def delete(cls, myid):
         product = Product.get_by_id(myid)
-        product_json = product.save(product)
+        product_json = Product.save(product)
         product.query.filter_by(id=myid).delete()
         db.session.commit()
         return product_json
@@ -119,7 +117,8 @@ class User(db.Model):
             'firstname': self.firstname,
             'lastname': self.lastname,
             'email': self.email,
-            'password_hash': self.password_hash
+            'password_hash': self.password_hash,
+            'roles': [role.name for role in self.roles]
         }
 
     def save_db(self):
@@ -143,29 +142,12 @@ class User(db.Model):
         return User.query.filter_by(username=myusername).first()
 
     @classmethod
-    def user_list(cls):
-        def to_json(user):
-            return {
-                'id': user.id,
-                'username': user.username,
-                'firstname': user.firstname,
-                'lastname': user.lastname,
-                'email': user.email,
-                'password_hash': user.password_hash
-            }
-
-        return {"users": [to_json(user) for user in User.query.all()]}
-
-    @classmethod
-    def delete(cls, myid):  # дороби ерори
-        try:
-            user = User.get_by_id(myid)
-            user_json = User.save(user)
-            User.query.filter_by(id=myid).delete()
-            db.session.commit()
-            return user_json
-        except Exception:
-            return
+    def delete(cls, myid):
+        user = User.get_by_id(myid)
+        user_json = User.save(user)
+        User.query.filter_by(id=myid).delete()
+        db.session.commit()
+        return user_json
 
 
 @app.route("/api/v1/hello-world-29")
@@ -175,85 +157,80 @@ def hello():
     return "Hello World 29"
 
 
-@app.route("/product", methods=['POST', 'PUT'])
+@app.route("/product", methods=['POST'])
 @handle_server_exception
 @auth.login_required(role='admin')
 def product():
-    if request.method == 'POST':  # +
-        pars = reqparse.RequestParser()
-        pars.add_argument('title', help='name cannot be blank', required=True)
-        pars.add_argument('text', help='name cannot be blank', required=True)
-        pars.add_argument('state', help='status cannot be blank', required=True)
-        pars.add_argument('category', help='status cannot be blank', required=True)
+    pars = reqparse.RequestParser()
+    pars.add_argument('title', help='name cannot be blank', required=True)
+    pars.add_argument('text', help='name cannot be blank', required=True)
+    pars.add_argument('state', help='status cannot be blank', required=True)
+    pars.add_argument('category', help='status cannot be blank', required=True)
 
-        data = pars.parse_args()
+    data = pars.parse_args()
 
-        title = data['title']
-        text = data['text']
-        state = data['state']
-        category = data['category']
+    title = data['title']
+    text = data['text']
+    state = data['state']
+    category = data['category']
 
-        product_1 = Product(
-            title=title,
-            text=text,
-            state=state,
-            category=category
-        )
-        try:
-            product_1.save_db()
-            return {'message': 'Product was successfully created'}, 200
-        except:
-            return {'message': 'Product name or description is alredy taken'}, 500
+    product_1 = Product(
+        title=title,
+        text=text,
+        state=state,
+        category=category
+    )
+    try:
+        product_1.save_db()
+        return {'message': 'Product was successfully created'}, 200
+    except:
+        return {'message': 'Product name or description is alredy taken'}, 500
 
 
-    elif request.method == 'PUT':  # +
-        pars = reqparse.RequestParser()
-        pars.add_argument('id', help='id cannot be blank', required=True)
-        pars.add_argument('title', help='title cannot be blank', required=True)
-        pars.add_argument('text', help='text cannot be blank', required=True)
-        pars.add_argument('state', help='state cannot be blank', required=True)
-        pars.add_argument('category', help='category cannot be blank', required=True)
+@app.route("/product", methods=['PUT'])
+@handle_server_exception
+@auth.login_required(role='admin')
+def update_product():
+    pars = reqparse.RequestParser()
+    pars.add_argument('id', help='id cannot be blank', required=True)
+    pars.add_argument('title', help='title cannot be blank', required=True)
+    pars.add_argument('text', help='text cannot be blank', required=True)
+    pars.add_argument('state', help='state cannot be blank', required=True)
+    pars.add_argument('category', help='category cannot be blank', required=True)
 
-        data = pars.parse_args()
-        id = int(data['id'])
+    data = pars.parse_args()
 
-        if not id:
-            return handle_error_format('Product with such id does not exist.',
-                                       'Field \'Id\' in path parameters.'), 400
+    id = int(data['id'])
 
-        try:
-            product_1 = Product.query.filter_by(id=id).first()
-            print(Product.save(product_1))
-            product_1.title = data['title']
-            product_1.text = data['text']
-            product_1.state = data['state']
-            product_1.category = data['category']
+    try:
+        product_1 = Product.query.filter_by(id=id).first()
+        product_1.title = data['title']
+        product_1.text = data['text']
+        product_1.state = data['state']
+        product_1.category = data['category']
 
-            db.session.commit()
-            return {'message': 'Product was successfully updated'}, 200
-        except:
-            return {'message': 'Product name or description is alredy taken'}, 500
+        db.session.commit()
+        return {'message': 'Product was successfully updated'}, 200
+    except:
+        return {'message': 'Product name or description is alredy taken'}, 500
 
 
 @app.route('/product/<int:product_id>', methods=['GET'])
 @handle_server_exception
 @auth.login_required(role='user')
 def product_by_id(product_id):
-    try:
-        product_1 = Product.get_by_id(product_id)
+    product_1 = Product.get_by_id(product_id)
 
-        if not product_1:
-            return handle_error_format('Product with such id does not exist.',
-                                       'Field \'Id\' in path parameters.'), 400
+    if not product_1:
+        return handle_error_format('Product with such id does not exist.',
+                                   'Field \'Id\' in path parameters.'), 400
 
-        return {'id': product_1.id,
-                'title': product_1.title,
-                'text': product_1.text,
-                'state': product_1.state,
-                'category': product_1.category
-                }, 200
-    except Exception:
-        return {'message': 'Error'}, 500
+    return {'id': product_1.id,
+            'title': product_1.title,
+            'text': product_1.text,
+            'state': product_1.state,
+            'category': product_1.category
+            }, 200
 
 
 @app.route('/product/<int:product_id>', methods=['DELETE'])
@@ -312,11 +289,11 @@ def create_user():
         user_1.save_db()
         return {"message": "User was successfully created"}, 200
     except Exception:
-        return {"message": "Username Or Email are alredy taken"}, 500
+        return {"message": "Username Or Email are already taken"}, 500
 
 
 @app.route('/user/<string:username>', methods=['PUT', 'DELETE'])
-# @handle_server_exception
+@handle_server_exception
 @auth.login_required(role='user')
 def user_by_nickname(username):
     username1 = auth.current_user()
--- src/test/all/test_main.py
+++ src/test/all/test_main.py
@@ -1,5 +1,5 @@
 from unittest import TestCase, mock
-from src.main import User, Product
+from src.main import User, Product, product, update_product, product_by_id, product_by_id2
 from src.main import User, Role
 from undecorated import undecorated
 from src.main import verify_password, get_user_roles, hello, Role
@@ -77,3 +77,253 @@ class TestRole(TestCase):
         result = Role.get_by_name('user')
 
         self.assertEqual(role, result)
+
+
+class TestProduct(TestCase):
+
+    def test_save(self):
+        product = Product(id=1, title="plug", text="goody",
+                          state="new", category="electronics")
+
+        expected_json = {'id': 1, 'title': 'plug', 'text': 'goody',
+                         'state': 'new', 'category': 'electronics'}
+
+        result = product.save()
+
+        self.assertEqual(expected_json, result)
+
+    @mock.patch('src.main.db.session.commit')
+    @mock.patch('src.main.db.session.add')
+    def test_save_db(self, mock_add, mock_commit):
+        product = Product(id=1, title="plug", text="goody",
+                          state="new", category="electronics")
+
+        mock_add.return_value = None
+        mock_commit.return_value = None
+
+        Product.save_db(product)
+
+        mock_add.assert_called_once_with(product)
+        mock_commit.assert_called_once_with()
+
+    @mock.patch('flask_sqlalchemy.model._QueryProperty.__get__')
+    def test_get_by_id(self, mock_query_property_getter):
+        product = Product(id=1, title="plug", text="goody",
+                          state="new", category="electronics")
+        mock_query_property_getter.return_value.filter_by.return_value.first.return_value = product
+
+        result = Product.get_by_id(1)
+
+        self.assertEqual(product, result)
+
+    @mock.patch('flask_sqlalchemy.model._QueryProperty.__get__')
+    def test_get_by_title(self, mock_query_property_getter):
+        product = Product(id=1, title="plug", text="goody",
+                          state="new", category="electronics")
+        mock_query_property_getter.return_value.filter_by.return_value.first.return_value = product
+
+        result = Product.get_by_title("plug")
+
+        self.assertEqual(product, result)
+
+    @mock.patch('src.main.db.session.commit')
+    @mock.patch('flask_sqlalchemy.model._QueryProperty.__get__')
+    @mock.patch('src.main.Product.get_by_id')
+    def test_delete(self, mock_get_by_id, mock_query_property_getter, mock_commit):
+        product = Product(id=1, title="plug", text="goody",
+                          state="new", category="electronics")
+        mock_get_by_id.return_value = product
+        mock_query_property_getter.return_value.filter_by.return_value.delete.return_value = None
+        mock_commit.return_value = None
+
+        result = Product.delete(1)
+
+        self.assertEqual(product.save(), result)
+        mock_get_by_id.assert_called_once_with(1)
+        mock_query_property_getter.return_value.filter_by.assert_called_once_with(id=1)
+        mock_query_property_getter.return_value.filter_by.return_value.delete.assert_called_once_with()
+        mock_commit.assert_called_once_with()
+
+
+class TestUser(TestCase):
+
+    def setUp(self) -> None:
+        self.user = User(
+            username='username',
+            firstname='firstname',
+            lastname='lastname',
+            email='email',
+            password_hash='password_hash'
+        )
+
+    def test_save(self):
+        user = self.user
+
+        expected_json = {
+            'email': 'email',
+            'firstname': 'firstname',
+            'id': None,
+            'lastname': 'lastname',
+            'password_hash': 'password_hash',
+            'roles': [],
+            'username': 'username'}
+
+        result = user.save()
+
+        self.assertEqual(expected_json, result)
+
+    @mock.patch('src.main.db.session.commit')
+    @mock.patch('src.main.db.session.add')
+    def test_save_db(self, mock_add, mock_commit):
+        user = self.user
+
+        mock_add.return_value = None
+        mock_commit.return_value = None
+
+        User.save_db(user)
+
+        mock_add.assert_called_once_with(user)
+        mock_commit.assert_called_once_with()
+
+    def test_create_hash(self):
+        user = self.user
+
+        result = user.create_hash('password_hash')
+
+        self.assertTrue(result)
+
+    def test_check_hash(self):
+        user = self.user
+        user.password = user.create_hash('password')
+
+        result = user.check_hash('password', user.password)
+
+        self.assertTrue(result)
+
+    @mock.patch('flask_sqlalchemy.model._QueryProperty.__get__')
+    def test_get_by_id(self, mock_query_property_getter):
+        user = self.user
+        user.id = 1
+        mock_query_property_getter.return_value.filter_by.return_value.first.return_value = user
+
+        result = User.get_by_id(1)
+
+        self.assertEqual(user, result)
+
+    @mock.patch('flask_sqlalchemy.model._QueryProperty.__get__')
+    def test_get_by_username(self, mock_query_property_getter):
+        user = self.user
+        mock_query_property_getter.return_value.filter_by.return_value.first.return_value = user
+
+        result = User.get_by_username('username')
+
+        self.assertEqual(user, result)
+
+    @mock.patch('src.main.db.session.commit')
+    @mock.patch('flask_sqlalchemy.model._QueryProperty.__get__')
+    @mock.patch('src.main.User.get_by_id')
+    def test_delete_2(self, mock_get_by_id, mock_query_property_getter, mock_commit):
+        mock_get_by_id.return_value = self.user
+        mock_query_property_getter.return_value.filter_by.return_value.delete.return_value = None
+        mock_commit.return_value = None
+
+        result = User.delete(1)
+
+        self.assertEqual(result, User.save(self.user))
+
+
+class TestProducts(TestCase):
+
+    def setUp(self) -> None:
+        self.product = Product(
+            id=1,
+            title='title',
+            text='text',
+            state='state',
+            category='category'
+        )
+
+        self.product_json_create = {
+            'id': 1,
+            'title': 'Plug',
+            'text': 'datext',
+            'state': 'new',
+            'category': 'electronics'
+        }
+
+        self.get_product_json = {
+            'id': 1,
+            'title': 'Plug',
+            'text': 'datext',
+            'state': 'new',
+            'category': 'electronics'
+        }
+
+        self.update_product_json = {
+            'id': 1,
+            'title': 'new',
+            'text': 'new',
+            'state': 'new',
+            'category': 'new'
+
+        }
+
+    @mock.patch('src.main.Product.save_db')
+    @mock.patch('flask_restful.reqparse.RequestParser.parse_args')
+    def test_product_create(self, mock_request_parser, mock_save_db):
+        mock_request_parser.return_value = self.product_json_create
+        mock_save_db.return_value = True
+        undecorated_product = undecorated(product)
+        result = undecorated_product()
+
+        self.assertEqual(({'message': 'Product was successfully created'}, 200), result)
+
+    @mock.patch('src.main.Product.save_db')
+    @mock.patch('src.main.Product.save')
+    @mock.patch('flask_restful.reqparse.RequestParser.parse_args')
+    def test_update_product(self, mock_request_parser, mock_save_to_db, mock_save):
+        mock_request_parser.return_value = self.update_product_json
+        mock_save_to_db.return_value = True
+        mock_save.return_value = Product.save(self.product)
+
+        undecorated_update_product = undecorated(update_product)
+        result = undecorated_update_product()
+
+        self.get_product_json['username'], self.get_product_json['first_name'], self.get_product_json[
+            'last_name'] = 'new', 'new', 'new'
+
+        self.assertEqual(({'message': 'Product name or description is alredy taken'}, 500), result)
+
+    @mock.patch('src.main.Product.get_by_id')
+    def test_product_by_id(self, mock_product_by_id):
+        mock_product_by_id.return_value = self.product
+
+        undecorated_product_by_id = undecorated(product_by_id)
+        result = undecorated_product_by_id(1)
+
+        self.assertEqual(({'category': 'category',
+                           'id': 1,
+                           'state': 'state',
+                           'text': 'text',
+                           'title': 'title'},
+                          200), result)
+
+    @mock.patch('src.main.Product.get_by_id')
+    def test_product_by_id_with_validation_error(self, mock_product_by_id):
+        mock_product_by_id.return_value = None
+
+        undecorated_product_by_id = undecorated(product_by_id)
+        result = undecorated_product_by_id(1)
+
+        self.assertEqual((
+            {'errors': [{'message': 'Product with such id does not exist.',
+                         'source': "Field 'Id' in path parameters."}],
+             'traceId': result[0].get('traceId')}, 400), result)
+
+    @mock.patch('src.main.Product.delete')
+    def test_product_by_id2(self, mock_delete):
+        mock_delete.return_value = self.get_product_json
+        undecorated_product_by_id2 = undecorated(product_by_id2)
+        result = undecorated_product_by_id2(1)
+
+        self.assertEqual({"message": f"Product not found"}, 404, result)
